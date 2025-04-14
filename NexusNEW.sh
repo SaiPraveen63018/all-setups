75fd5de259aa150d3c4fcca593ca85d5ff5b8d68
SaiPraveen63018 <saipraveendonthamsetti@gmail.com>
2025-04-14 11:35:58 +0530

    Update NexusNEW.sh

--- NexusNEW.sh
+++ NexusNEW.sh
@@ -6,3 +6,7 @@ useradd nexus
 chown -R nexus:nexus nexus-3.79.1-04 sonatype-work
 cd nexus-3.79.1-04/bin/
 sudo echo "run_as_user="nexus"" > /opt/nexus-3.79.1-04/bin/
+su - nexus
+cd /opt/
+cd nexus-3.79.1-04/bin/
+./nexus start
