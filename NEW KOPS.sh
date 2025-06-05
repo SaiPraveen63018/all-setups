ed8adbaaa3a37b59780262859f8f2494a9a28e0c
SaiPraveen63018 <saipraveendonthamsetti@gmail.com>
2025-06-05 11:11:32 +0530

    Update NEW KOPS.sh

--- NEW KOPS.sh
+++ NEW KOPS.sh
@@ -2,3 +2,4 @@ curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s htt
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+kops create cluster --name praveen.k8s.local --zones us-east-1a,us-east-1b,us-east-1c --master-count=1 --master-size t2.medium --master-volume-size 30 --node-count=2 --node-size t2.micro --node-volume-size 30 
