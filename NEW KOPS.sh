1c29ea4717da896300a032683df7150a362e01ce
SaiPraveen63018 <saipraveendonthamsetti@gmail.com>
2025-06-07 13:13:08 +0530

    Update NEW KOPS.sh

--- NEW KOPS.sh
+++ NEW KOPS.sh
@@ -2,13 +2,19 @@
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
 
+
+#! /bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
-
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+
 kops create cluster --name praveen.k8s.local --zones us-east-1a,us-east-1b,us-east-1c --control-plane-count=1 --control-plane-size t2.medium --control-plane-volume-size 30 --node-count=2 --node-size t2.micro --node-volume-size 30 
